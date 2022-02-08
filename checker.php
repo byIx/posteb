35adca13beda1009f1138e00859d6a9ace2db6f0
byIx <40878600+byIx@users.noreply.github.com>
2022-02-08 23:55:28 +0600

    Update checker.php

--- checker.php
+++ checker.php
@@ -1,1 +1,1 @@
-{ "version":"1.0.0", "longversion":10000, "released":1644332400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/111\/" }
+{ "version":"1.0.0", "longversion":10000, "released":1644332400, "updateurl":"https:\/\/ipbmafia.ru\/files\/file\/2629-k4-post-editbutton\/" }
